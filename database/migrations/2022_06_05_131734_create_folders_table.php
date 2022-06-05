20a4f6ea60db4a0d5a02fccb7a9caa1a7e1634f9
saku-sasaki <hisako.sasaki1986@icloud.com>
2022-06-05 20:42:12 +0900

    チュートリアル Todoアプリ タスク一覧まで完成

--- database/migrations/2022_06_05_131734_create_folders_table.php
+++ database/migrations/2022_06_05_131734_create_folders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFolersTable extends Migration
+class CreateFoldersTable extends Migration
 {
     /**
      * Run the migrations.
